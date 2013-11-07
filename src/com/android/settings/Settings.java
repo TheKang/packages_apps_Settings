0ccf797ed96e5c12b9eeb3b75eb601325a04cbed
Danny Baumann <dannybaumann@web.de>
2013-11-07 09:42:29 +0100

    Settings: Add CM Profiles (2/2)
    Autor:    Danny Baumann <dannybaumann@web.de>
    
    	modified:   AndroidManifest.xml
    	new file:   res/drawable-hdpi/ic_location.png
    	new file:   res/drawable-hdpi/ic_menu_add.png
    	new file:   res/drawable-hdpi/ic_menu_nfc_writer_dark.png
    	new file:   res/drawable-hdpi/ic_menu_trash_holo_dark.png
    	new file:   res/drawable-hdpi/ic_settings_profiles.png
    	new file:   res/drawable-hdpi/nfc_writer.png
    	new file:   res/drawable-mdpi/ic_location.png
    	new file:   res/drawable-mdpi/ic_menu_add.png
    	new file:   res/drawable-mdpi/ic_menu_trash_holo_dark.png
    	new file:   res/drawable-mdpi/ic_settings_profiles.png
    	new file:   res/drawable-xhdpi/ic_location.png
    	new file:   res/drawable-xhdpi/ic_menu_add.png
    	new file:   res/drawable-xhdpi/ic_menu_nfc_writer_dark.png
    	new file:   res/drawable-xhdpi/ic_menu_trash_holo_dark.png
    	new file:   res/drawable-xhdpi/ic_settings_profiles.png
    	new file:   res/drawable-xhdpi/nfc_writer.png
    	new file:   res/layout/nfc_select.xml
    	new file:   res/layout/nfc_writer.xml
    	new file:   res/layout/preference_name.xml
    	new file:   res/layout/preference_profiles.xml
    	new file:   res/layout/preference_streamvolume.xml
    	new file:   res/layout/profile_name_dialog.xml
    	new file:   res/layout/profile_tabs.xml
    	modified:   res/values/kang_arrays.xml
    	modified:   res/values/kang_strings.xml
    	modified:   res/values/styles.xml
    	modified:   res/xml/additional_settings.xml
    	new file:   res/xml/appgroup_list.xml
    	new file:   res/xml/application_list.xml
    	new file:   res/xml/profile_config.xml
    	new file:   res/xml/profile_settings.xml
    	new file:   res/xml/profiles_settings.xml
    	modified:   res/xml/recent_panel_settings.xml
    	modified:   res/xml/settings_headers.xml
    	modified:   src/com/android/settings/Settings.java
    	modified:   src/com/android/settings/Utils.java
    	new file:   src/com/android/settings/profiles/AbstractTriggerPreference.java
    	new file:   src/com/android/settings/profiles/AppGroupConfig.java
    	new file:   src/com/android/settings/profiles/AppGroupList.java
    	new file:   src/com/android/settings/profiles/ApplicationItemPreference.java
    	new file:   src/com/android/settings/profiles/BluetoothTriggerPreference.java
    	new file:   src/com/android/settings/profiles/NFCProfile.java
    	new file:   src/com/android/settings/profiles/NFCProfileSelect.java
    	new file:   src/com/android/settings/profiles/NFCProfileUtils.java
    	new file:   src/com/android/settings/profiles/NFCProfileWriter.java
    	new file:   src/com/android/settings/profiles/NamePreference.java
    	new file:   src/com/android/settings/profiles/ProfileAirplaneModePreference.java
    	new file:   src/com/android/settings/profiles/ProfileConfig.java
    	new file:   src/com/android/settings/profiles/ProfileConnectionPreference.java
    	new file:   src/com/android/settings/profiles/ProfileEnabler.java
    	new file:   src/com/android/settings/profiles/ProfileGroupConfig.java
    	new file:   src/com/android/settings/profiles/ProfileRingModePreference.java
    	new file:   src/com/android/settings/profiles/ProfileRingtonePreference.java
    	new file:   src/com/android/settings/profiles/ProfilesList.java
    	new file:   src/com/android/settings/profiles/ProfilesPreference.java
    	new file:   src/com/android/settings/profiles/ProfilesSettings.java
    	new file:   src/com/android/settings/profiles/StreamVolumePreference.java
    	new file:   src/com/android/settings/profiles/TriggersFragment.java
    	new file:   src/com/android/settings/profiles/WifiTriggerAPPreference.java
    
    Change-Id: I049eaf5deccc1bf173d4625a295c332c8abe45df

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -92,6 +92,10 @@ import com.android.settings.slim.quicksettings.QuickSettingsTiles;
 import com.android.settings.slim.ShakeEvents;
 import com.android.settings.slim.QuietHours;
 import com.android.settings.slim.themes.ThemeEnabler;
+import com.android.settings.profiles.AppGroupConfig;
+import com.android.settings.profiles.ProfileConfig;
+import com.android.settings.profiles.ProfileEnabler;
+import com.android.settings.profiles.ProfilesSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
@@ -548,6 +552,9 @@ public class Settings extends PreferenceActivity
                 BlacklistSettings.class.getName().equals(fragmentName) ||
                 BluetoothSettings.class.getName().equals(fragmentName) ||
                 DreamSettings.class.getName().equals(fragmentName) ||
+                ProfilesSettings.class.getName().equals(fragmentName) ||
+                ProfileConfig.class.getName().equals(fragmentName) ||
+                AppGroupConfig.class.getName().equals(fragmentName) ||
                 LocationSettings.class.getName().equals(fragmentName) ||
                 ToggleAccessibilityServicePreferenceFragment.class.getName().equals(fragmentName) ||
                 PrintSettingsFragment.class.getName().equals(fragmentName) ||
@@ -846,6 +853,7 @@ public class Settings extends PreferenceActivity
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
         public static ThemeEnabler mThemeEnabler;
+        private final ProfileEnabler mProfileEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
 
@@ -866,7 +874,8 @@ public class Settings extends PreferenceActivity
                 return HEADER_TYPE_CATEGORY;
             } else if (header.id == R.id.wifi_settings
                     || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.theme_settings) {
+                    || header.id == R.id.theme_settings
+                    || header.id == R.id.profiles_settings) {
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.security_settings) {
                 return HEADER_TYPE_BUTTON;
@@ -913,6 +922,7 @@ public class Settings extends PreferenceActivity
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
             mThemeEnabler = new ThemeEnabler(context, new Switch(context));
+            mProfileEnabler = new ProfileEnabler(context, new Switch(context));
             mDevicePolicyManager = dpm;
         }
 
@@ -986,6 +996,8 @@ public class Settings extends PreferenceActivity
                         mBluetoothEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.theme_settings) {
                         mThemeEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.profiles_settings) {
+                        mProfileEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
@@ -1060,12 +1072,14 @@ public class Settings extends PreferenceActivity
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
             mThemeEnabler.resume();
+            mProfileEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
             mThemeEnabler.resume();
+            mProfileEnabler.pause();
         }
     }
 
@@ -1193,6 +1207,7 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class ProfilesSettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class NotificationStationActivity extends Settings { /* empty */ }
     public static class UserSettingsActivity extends Settings { /* empty */ }
