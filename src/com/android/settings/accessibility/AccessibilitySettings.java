a1ae335f58eda07dc12efd8ccedf3dc8c16dc536
Schubi <schubi@erlangen.ccc.de>
2014-06-03 11:27:53 +0200

    Fix AccessibilitySettings
    Change-Id: Ie85caf880a103deb883b807b10f5b53b51cd8f59

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -57,6 +57,7 @@ import com.android.internal.view.RotationPolicy.RotationPolicyListener;
 import com.android.settings.DialogCreatable;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import java.util.HashSet;
 import java.util.List;
